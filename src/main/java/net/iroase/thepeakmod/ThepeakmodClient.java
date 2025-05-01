bb7a37b6e3af7eb2dd636350f179713b2aef6727
iroase <younesse.talib75@gmail.com>
2025-05-01 22:14:53 +0200

    rts

---
+++ src/main/java/net/iroase/thepeakmod/ThepeakmodClient.java
@@ -0,0 +1,10 @@
+package net.iroase.thepeakmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThepeakmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
